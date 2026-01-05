0a48f6ea2227c9cee93bba2e8153bc5487a7c3ed
Naveen Yasa <yasanaveen535@gmail.com>
2026-01-05 16:01:07 +0530

    new

--- src/main/java/pageObjects/UpdateScreenObjects.java
+++ src/main/java/pageObjects/UpdateScreenObjects.java
@@ -5,15 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utils.WaitUtill;
 
 public class UpdateScreenObjects {
 
     private WebDriver driver;
+    private WaitUtill waitutil;
 
-    public UpdateScreenObjects(WebDriver driver)
-    {
-         this.driver=driver;
-        PageFactory.initElements(driver,this);
+    public UpdateScreenObjects(WebDriver driver) {
+        this.driver = driver;
+        waitutil = new WaitUtill(driver);
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "(//*[text()='Update'])[1]")
@@ -23,25 +25,19 @@ public class UpdateScreenObjects {
     WebElement applicationFeefield;
 
 
-
-    public void clkUpdateBtn()
-    {
+    public void clkUpdateBtn() {
         updateBtn.click();
     }
 
-    public boolean applicatinFeefield()
-    {
-        try{
-            applicationFeefield.click();
+    public boolean applicatinFeefield() {
+        try {
+            waitutil.waitForClickable(applicationFeefield).click();
             return true;
-        }catch (TimeoutException e)
-        {
+        } catch (TimeoutException e) {
             return false;
         }
 
     }
-
-
 
 
 }
